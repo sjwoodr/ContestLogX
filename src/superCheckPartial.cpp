a1faf49ba5d8614cae411a6cb4e05d9370210e8f
Steve Woodruff <steve@n9oh.com>
2026-02-24 16:33:37 -0500

    Add GPL3 license
    - Added LICENSE file (GNU General Public License v3)
    - Added GPL3 preamble to all 97 .cpp and .h source files

--- src/superCheckPartial.cpp
+++ src/superCheckPartial.cpp
@@ -1,6 +1,21 @@
 /*
  * ContestLogX - Amateur Radio Contest Logging Software
  * Copyright (c) 2025-2026, by Steve Woodruff, N9OH
+ *
+ * This file is part of ContestLogX.
+ *
+ * ContestLogX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ContestLogX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ContestLogX.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "superCheckPartial.h"
